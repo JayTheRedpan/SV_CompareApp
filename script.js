1567bac52a3dfc41e5af7c8db9ce33ef0fa8129d
JayTheRedpan <120038210+JayTheRedpan@users.noreply.github.com>
2022-12-07 15:45:03 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var siteurl = "https://jaytheredpan.github.io/SV_CompareApp/";
+
+function updateImg1(){
+    document.getElementById('char1img').src = siturl + "Images/" + document.getElementById('char1select').value;
+}
+
+function updateImg2(){
+    document.getElementById('char2img').src = siturl + "Images/" + document.getElementById('char2select').value;
+}
