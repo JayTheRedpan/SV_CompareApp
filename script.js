e3d675b6f6dec48157f3207a5c7618a1b96e40d0
JayTheRedpan <120038210+JayTheRedpan@users.noreply.github.com>
2022-12-08 14:09:35 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
+ const charSize = 1;
+ 
  //pull all character data from characters json
 async function getCharData() {
     const response = await fetch('./characters.json');
     
     return response.json();
+}
+
+//on page load
+window.onload = function() {
+    loadCharacters();
+  };
+
+//update drop downs with characters from json
+async function loadCharacters(){
+    const characters = await getCharData();
+    const select1 = document.getElementById('char1select');
+    const select2 = document.getElementById('char2select');
+
+    for (var i=0 ; i < characters.length ; i++)
+    {
+        var opt1 = document.createElement('option');
+        opt1.value = characters[i].name;
+        opt1.text = characters[i].name[0].toUpperCase() + characters[i].name.substring(1);
+        select1.add(opt1);
+
+        var opt2 = document.createElement('option');
+        opt2.value = characters[i].name;
+        opt2.text = characters[i].name[0].toUpperCase() + characters[i].name.substring(1);
+        select2.add(opt2);
+    }
 }
 
 //updates character 1 after a selection is made
@@ -30,7 +57,7 @@ async function updateChar1(){
 
     //update ref images and size according to character size
     refImg.src = "./images/ref/" + charName + ".png";
-    refImg.style.height = (5 * charData.height) + 'px';
+    refImg.style.height = (document.getElementById('sizeselect').value * charData.height) + 'px';
 
 }
 
@@ -59,6 +86,6 @@ async function updateChar2(){
 
     //update ref images and size according to character size
     refImg.src = "./images/ref/" + charName + ".png";
-    refImg.style.height = (5 * charData.height) + 'px';
+    refImg.style.height = (document.getElementById('sizeselect').value * charData.height) + 'px';
 
 }
