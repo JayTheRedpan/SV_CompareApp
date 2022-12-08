b5ad2da4f814ccfef56870f8ff88cd7328b1d387
JayTheRedpan <120038210+JayTheRedpan@users.noreply.github.com>
2022-12-08 01:23:31 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,64 @@
-//var siteurl = "https://jaytheredpan.github.io/SV_CompareApp/";
-var siteurl = "./";
-
-function updateImg1(){
-    document.getElementById('char1img').src = siteurl + "Images/" + document.getElementById('char1select').value;
+ //pull all character data from characters json
+async function getCharData() {
+    const response = await fetch('./characters.json');
+    
+    return response.json();
 }
 
-function updateImg2(){
-    document.getElementById('char2img').src = siteurl + "Images/" + document.getElementById('char2select').value;
+//updates character 1 after a selection is made
+async function updateChar1(){
+    //variable declaration
+    var charName = document.getElementById('char1select').value;
+    const characters = await getCharData();
+    var charData;
+
+    var profileImg = document.getElementById('profile1img');
+    var refImg = document.getElementById('ref1img');
+
+    //search through character json for selected character
+    for (var i=0 ; i < characters.length ; i++)
+    {
+        if (characters[i]["name"] == charName) {
+            charData = characters[i];
+        }
+    }
+    console.log(charData);
+    
+    //update profile images and size according to character size
+    profileImg.src = "./images/head/" + charName + ".png";
+    //profileImg.style.width = 
+
+    //update ref images and size according to character size
+    refImg.src = "./images/ref/" + charName + ".png";
+    refImg.style.height = (5 * charData.height) + 'px';
+
+}
+
+//updates character 2 after a selection is made
+async function updateChar2(){
+    //variable declaration
+    var charName = document.getElementById('char2select').value;
+    const characters = await getCharData();
+    var charData;
+
+    var profileImg = document.getElementById('profile2img');
+    var refImg = document.getElementById('ref2img');
+
+    //search through character json for selected character
+    for (var i=0 ; i < characters.length ; i++)
+    {
+        if (characters[i]["name"] == charName) {
+            charData = characters[i];
+        }
+    }
+    console.log(charData);
+    
+    //update profile images and size according to character size
+    profileImg.src = "./images/head/" + charName + ".png";
+    //profileImg.style.width = 
+
+    //update ref images and size according to character size
+    refImg.src = "./images/ref/" + charName + ".png";
+    refImg.style.height = (5 * charData.height) + 'px';
+
 }
